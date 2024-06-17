f9c6fcee6026df6d9f891b79f37b0f0cc9a9e05b
rufevean <deerajmydearalways@gmail.com>
2024-06-17 14:34:29 +0530

    minor changes

--- r.md
+++ r.md
@@ -1,1 +1,40 @@
-# TEST MARKDOWN PAGE
+## Tools i use 
+
+- [Neovim](https://neovim.io/) - The best Text Editor 
+- [Tmux](https://github.com/tmux/tmux/wiki) - A terminal multiplexer
+- [Wezterm](https://wezfurlong.org/wezterm/) - A terminal emulator
+- [Spiceify](https://spicetify.app/) - Customizing Spotify 
+- [Zsh](https://ohmyz.sh/) - A shell with oh-my-zsh for plugins  
+
+--- 
+## My Dotfiles
+
+- [i3 , Zsh, wezterm] (https://github.com/rufevean/dotfiles)
+- [Nvim] (https://github.com/rufevean/nvim)
+
+## Good Repositories
+
+- [Awesome-Machine-Learning-In-Compilers](https://github.com/zwang4/awesome-machine-learning-in-compilers) - A curated list of awesome machine learning techniques in compilers research.
+- [Awesome-Neovim](https://github.com/rockerBOO/awesome-neovim) - collection of awesome neovim plugins
+- [lowlevelprogramming-university](https://github.com/gurugio/lowlevelprogramming-university) - How to be low-level programmer
+- [Build-your-own-x](https://github.com/codecrafters-io/build-your-own-x) - Build your own (insert technology here) , realy good for practical learning.
+
+
+## Good websites if you want to learn about compilers
+
+- [Crafting Interpreters](https://craftinginterpreters.com/) - A handbook for making programming languages
+- [Awesome-Compilers](https://github.com/aalhour/awesome-compilers) - A curated list of awesome resources on compilers, interpreters, and runtimes.
+- [LLVM](https://llvm.org/) - The LLVM Compiler Infrastructure Project
+- [Compiler Explorer](https://godbolt.org/) - An interactive compiler 
+- [ The compiler writer resource page](http://c9x.me/compile/bib/) - A list of resources for compiler writers
+
+
+## Languages i recommend 
+
+- [C](https://en.wikipedia.org/wiki/C_(programming_language))
+- [Rust](https://www.rust-lang.org/)
+- [Forth](https://en.wikipedia.org/wiki/Forth_(programming_language))
+- [Bash](https://en.wikipedia.org/wiki/Bash_(Unix_shell))
+
+
+
--- src/MarkdownPage.jsx
+++ src/MarkdownPage.jsx
@@ -1,22 +1,52 @@
 
-import React, { useEffect, useState } from 'react';
-import ReactMarkdown from 'react-markdown';
+import React from 'react';
+import './MarkdownPage.css';
 
 function MarkdownPage() {
-  const [markdown, setMarkdown] = useState('');
-
-  useEffect(() => {
-    // Fetch or import your markdown file here
-    fetch('r.md')
-      .then(response => response.text())
-      .then(text => setMarkdown(text))
-      .catch(error => console.error('Error loading markdown:', error));
-  }, []);
-
   return (
-    <div>
+    <div className="markdown-container">
       <h1>Resources</h1>
-      <ReactMarkdown>{markdown}</ReactMarkdown>
+
+      <h2>Tools I Use</h2>
+      <ul>
+        <li><a href="https://neovim.io/">Neovim</a> - The best Text Editor</li>
+        <li><a href="https://github.com/tmux/tmux/wiki">Tmux</a> - A terminal multiplexer</li>
+        <li><a href="https://wezfurlong.org/wezterm/">Wezterm</a> - A terminal emulator</li>
+        <li><a href="https://spicetify.app/">Spiceify</a> - Customizing Spotify</li>
+        <li><a href="https://ohmyz.sh/">Zsh</a> - A shell with oh-my-zsh for plugins</li>
+      </ul>
+
+
+      <h2>My Dotfiles</h2>
+      <ul>
+        <li><a href="https://github.com/rufevean/dotfiles">i3, Zsh, wezterm</a></li>
+        <li><a href="https://github.com/rufevean/nvim">Nvim</a></li>
+      </ul>
+
+      <h2>Good Repositories</h2>
+      <ul>
+        <li><a href="https://github.com/zwang4/awesome-machine-learning-in-compilers">Awesome-Machine-Learning-In-Compilers</a> - A curated list of awesome machine learning techniques in compilers research.</li>
+        <li><a href="https://github.com/rockerBOO/awesome-neovim">Awesome-Neovim</a> - collection of awesome neovim plugins</li>
+        <li><a href="https://github.com/gurugio/lowlevelprogramming-university">lowlevelprogramming-university</a> - How to be low-level programmer</li>
+        <li><a href="https://github.com/codecrafters-io/build-your-own-x">Build-your-own-x</a> - Build your own (insert technology here), really good for practical learning.</li>
+      </ul>
+
+      <h2>Good Websites if You Want to Learn About Compilers</h2>
+      <ul>
+        <li><a href="https://craftinginterpreters.com/">Crafting Interpreters</a> - A handbook for making programming languages</li>
+        <li><a href="https://github.com/aalhour/awesome-compilers">Awesome-Compilers</a> - A curated list of awesome resources on compilers, interpreters, and runtimes.</li>
+        <li><a href="https://llvm.org/">LLVM</a> - The LLVM Compiler Infrastructure Project</li>
+        <li><a href="https://godbolt.org/">Compiler Explorer</a> - An interactive compiler</li>
+        <li><a href="http://c9x.me/compile/bib/">The compiler writer resource page</a> - A list of resources for compiler writers</li>
+      </ul>
+
+      <h2>Languages I Recommend</h2>
+      <ul>
+        <li><a href="https://en.wikipedia.org/wiki/C_(programming_language)">C</a></li>
+        <li><a href="https://www.rust-lang.org/">Rust</a></li>
+        <li><a href="https://en.wikipedia.org/wiki/Forth_(programming_language)">Forth</a></li>
+        <li><a href="https://en.wikipedia.org/wiki/Bash_(Unix_shell)">Bash</a></li>
+      </ul>
     </div>
   );
 }
